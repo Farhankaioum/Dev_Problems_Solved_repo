417dd76d82db1d9f73f2c016c908426b25fd2ca8
Farhan Kaioum <km1472233@gmail.com>
2020-12-15 00:06:45 +0600

    Selection sort implemented

---
+++ qs/QS/SelectionSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+            foreach (var a in arr)
+            {
+                Console.Write(a + " ");
+            }
+            Console.WriteLine();
+
+            for (int i= 0; i < arr.Length - 1; i++)
+            {
+                var minValuePosition = i;
+
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minValuePosition])
+                        minValuePosition = j;
+                }
+
+                var temp = arr[i];
+                arr[i] = arr[minValuePosition];
+                arr[minValuePosition] = temp;
+            }
+
+            foreach (var a in arr)
+            {
+                Console.Write(a + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
