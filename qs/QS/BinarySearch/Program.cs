626df3aab517478fe5afd89696683f2543605ac6
Farhan Kaioum <km1472233@gmail.com>
2020-12-15 01:24:45 +0600

    binary search implemented

---
+++ qs/QS/BinarySearch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            Array.Sort(arr);
+            var value = 1;
+            int foundValue = 0;
+
+            int start = 0, length = arr.Length - 1;
+
+            while (start <= length)
+            {
+                int mid = start + length / 2;
+
+                if (arr[mid] == value)
+                {
+                    foundValue = arr[mid];
+                    break;
+                }
+
+                if (arr[mid] < value) // ignore left side values
+                    start = mid + 1;
+
+                else
+                    length = mid - 1;
+            }
+
+            Console.WriteLine(foundValue == value ? "Value found" : "Not present in this array!");
+        }
+    }
+}
