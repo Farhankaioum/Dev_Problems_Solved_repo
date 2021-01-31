94aa58ab54eb056fd6a63f874ca67147e2ee7ebb
Farhan Kaioum <km1472233@gmail.com>
2021-01-31 13:12:34 +0600

    array practice

---
+++ qs/QS/DT-1/ArrayDemo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DT_1
+{
+    public class ArrayDemo
+    {
+        private int[] array;
+        private int count = 0;
+
+        public ArrayDemo(int size)
+        {
+            array = new int[size];
+        }
+
+        public void Insert(int value)
+        {
+            if(array.Length == count)
+            {
+                var newArray = new int[count * 2];
+
+                for (int i = 0; i < count; i++)
+                {
+                    newArray[i] = array[i];
+                }
+
+                array = newArray;
+            }
+
+            array[count++] = value;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new IndexOutOfRangeException();
+
+            for (int i = index; i < count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            count--;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public int IndexOf(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new IndexOutOfRangeException();
+
+            return array[index];
+        }
+    }
+}
---
+++ qs/QS/DT-1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DT_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // arraylist
+            var arrayDemo = new ArrayDemo(2);
+            arrayDemo.Insert(10);
+            arrayDemo.Insert(20);
+            arrayDemo.Insert(30);
+            arrayDemo.Insert(40);
+
+            arrayDemo.RemoveAt(1);
+
+            arrayDemo.Print();
+        }
+    }
+}
