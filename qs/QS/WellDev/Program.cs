d114ee19eb19985c4394c2dca6a32bcb769ce771
Farhankaioum <km1472233@gmail.com>
2021-03-16 22:45:33 +0600

    welldev problem solved

---
+++ qs/QS/WellDev/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WellDev
+{
+    class Program
+    {
+        static int recurse(int a, int b)
+        {
+            if (a % b == 2)
+                return a;
+            else
+                return recurse(a + b, a-b);
+        }
+
+        static void Main(string[] args)
+        {
+            var value = recurse(7, 2);
+            Console.WriteLine(value);
+        }
+    }
+}
